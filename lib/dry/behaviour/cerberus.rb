19ab215af050ffbf06727b43aa06856eacc220b4
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2017-04-12 06:56:45 +0200

    Codeblocks are handled gracefully.

--- lib/dry/behaviour/cerberus.rb
+++ lib/dry/behaviour/cerberus.rb
@@ -93,10 +93,13 @@ module Dry
                                         end
               next if hash.nil?
               hash.all? do |param, condition|
-                idx = m.parameters.index { |_type, var| var == param }
+                type_var, idx = m.parameters.each_with_index.detect { |(_type, var), idx| var == param }
                 # rubocop:disable Style/CaseEquality
                 # rubocop:disable Style/RescueModifier
-                idx && condition === args[idx] rescue false # FIXME: more accurate
+                idx && (case type_var.first
+              when :req, :opt then condition === args[idx] rescue false
+                        when :block then condition === cb
+                        end)
                 # rubocop:enable Style/RescueModifier
                 # rubocop:enable Style/CaseEquality
               end
--- spec/dry/guards_spec.rb
+++ spec/dry/guards_spec.rb
@@ -18,7 +18,7 @@ describe Dry::Guards do
     expect(gt.a(42)).to eq(2)
     expect(gt.a(3.14)).to eq(3)
     expect(gt.a(3)).to eq(4)
-    # expect(gt.a('Hello', &-> { puts 0 })).to eq(5) NYI
+    expect(gt.a('Hello', &-> { puts 0 })).to eq(5)
     expect(gt.a(*%w|1 2 3|)).to eq(6)
     expect(gt.a('Hello')).to eq('ALL')
   end
